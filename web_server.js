e3a7548f01ff5ad43d6a95720e7d3f30199f7e98
pallalarajasekar@gmail.com <pallalarajasekar@gmail.com>
2016-01-30 16:42:13 +0530

    some css files added

--- web_server.js
+++ web_server.js
@@ -25,3 +25,35 @@ app.get('/beginner/apply', function(req, res){
 app.get('/beginner/beginner', function(req, res) {
     res.render("beginner/beginner")
 })
+
+app.get('/intermediate/intermediate', function(req, res) {
+    res.render("intermediate/intermediate")
+})
+
+app.get('/intermediate/intermediate1', function(req, res) {
+    res.render("intermediate/intermediate1")
+})
+
+app.get('/Advanced/advanced', function(req, res) {
+    res.render("Advanced/advanced")
+})
+app.get('/Advanced/htmldogexradius', function(req, res) {
+    res.render("Advanced/htmldogexradius")
+})
+
+app.get('/Advanced/position', function(req, res) {
+    res.render("Advanced/position")
+})
+app.get('/Advanced/selector', function(req, res) {
+    res.render("Advanced/selector")
+})
+app.get('/Advanced/advancedcolors', function(req, res) {
+    res.render("Advanced/advancedcolors")
+})
+
+app.get('/Advanced/media', function(req, res) {
+    res.render("Advanced/media")
+})
+app.get('/Advanced/transform', function(req, res) {
+    res.render("Advanced/transform")
+})
